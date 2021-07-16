b92d10fa001fe9fd9c68c5581670284e1b46fb8d
Jonathan <jonathan@indirectproof.net>
2021-07-16 15:31:33 -0400

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "indirectproof/fractals/pkg/julia"
+
+func main() {
+	fractal := julia.NewJuliaFractal()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jwelch1324/gofractal/v2
+
+go 1.16
